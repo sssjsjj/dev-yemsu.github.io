2ba1fb2240c4e971639a04b515abbfcd4ce96da4
정수진 <sssjsjj@ipartners.co.kr>
2022-02-16 14:35:25 +0900

    update: 파비콘, 타이틀 설정

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   publicPath: '/resume',
   outputDir: 'docs',
-  // css: {
-  //   loaderOptions: {
-  //     scss: {
-  //       additionalData: ``
-  //     }
-  //   }
-  // }
 }
