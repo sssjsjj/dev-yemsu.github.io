ebcf8d37dc0c3af9ba487085c7b697a218b172e7
정수진 <sssjsjj@ipartners.co.kr>
2022-03-04 19:42:22 +0900

    feat: route param에 따라 컨텐츠 삽입 기능 추가

--- src/router/views/PostDetail.vue
+++ src/router/views/PostDetail.vue
@@ -1,13 +1,24 @@
 <template>
-  <div> Post Detail </div>
+  <div v-html="contents"></div>
 </template>
 
 <script>
-export default {
+import htmlConverter from "@/utils/htmlConverter";
 
+export default {
+  data() {
+    return {
+      contents: null
+    }
+  },
+  async created() {
+    try {
+      const param = this.$route.params.title
+      const post = await import(`@/contents/${param}.md`)
+      this.contents = htmlConverter(post.default)
+    } catch (e) {
+      console.log(e)
+    }
+  },
 }
 </script>
-
-<style>
-
-</style>
