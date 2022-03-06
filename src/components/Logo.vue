1d852e7d550940d3dbfd8f70421dd50426537874
정수진 <sssjsjj@ipartners.co.kr>
2022-03-06 16:17:52 +0900

    feat: create LinkGithub, Logo, Header comp

---
+++ src/components/Logo.vue
@@ -0,0 +1,52 @@
+<template>
+  <a href="/" :class="`logo size-${size}`">
+    <span class="logo-text dev">
+      D<span class="hidden">E</span>V
+    </span>
+    <span class="logo-text enjoy">
+      <span class="common-e">E</span>NJOY🌊
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: 'medium'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  display: block;
+  position: relative;
+  font-weight: 500;
+  transform: rotate(-18deg);
+  &.size-medium {
+    font-size: 20px;
+  }
+
+  .emoji-main {
+    font-size: 50px;
+  }
+  .logo-text {
+    &.dev {
+      margin-top: -1.05em;
+      margin-right: -1.1em;
+      writing-mode: vertical-rl;
+      text-orientation: upright;
+    }
+    &.enjoy {
+      letter-spacing: 0.1em;
+      color: rgb(111, 0, 255);
+    } 
+    .hidden {
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <h1><Logo :size="'medium'" /></h1>
+    <!-- <nav>
+      <ul>
+        <li><a href="">menu 1</a></li>
+        <li><a href="">menu 2</a></li>
+        <li><a href="">menu 3</a></li>
+        <li><a href="">menu 4</a></li>
+      </ul>
+    </nav> -->
+    <div class="links">
+      <LinkGithub />
+    </div>
+  </header>
+</template>
+
+<script>
+import Logo from '@/components/Logo.vue'
+import LinkGithub from '@/components/LinkGithub.vue'
+
+export default {
+  components: {
+    Logo,
+    LinkGithub
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px 50px 30px;
+}
+nav {
+  ul {
+    display: flex;
+    gap: 20px;
+  }
+}
+</style>
