97b19d910881d838e22d30811bf6d77b600e82cf
정수진 <sssjsjj@ipartners.co.kr>
2022-03-06 12:41:32 +0900

    feat: post list data binding, style

--- src/router/views/PostList.vue
+++ src/router/views/PostList.vue
@@ -1,17 +1,49 @@
 <template>
-  <div>
-    <div>
-      post list
-    </div>
+  <div class="container">
+    <main>
+      <div class="list-post">
+        <div
+          v-for="(post, i) in postMap"
+          :key="`post${i}`"
+          class="post"
+        >
+          <a :href="post.name" :title="post.title">
+            <h4 class="title" v-html="post.title"></h4>
+            <p class="description">{{ post.description }}</p>
+            <p class="date">{{ post.date }}</p>
+          </a>
+          <ul class="keywords">
+            <li
+              v-for="(keyword, keywordIndex) in post.keywords"
+              :key="`keyword${keywordIndex}`"
+              class="keyword"
+            >
+              {{ keyword }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-export default {
+import posts from '@/contents/posts'
 
+export default {
+  data() {
+    return {
+      postMap: []
+    }
+  },
+  created() {
+    this.postMap = JSON.parse(posts)
+  },
+  methods: {
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import '@/assets/style/PostList.scss';
 </style>
