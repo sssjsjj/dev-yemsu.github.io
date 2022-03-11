b8ae4b677179773019cc720644d98e72ee20b756
정수진 <sssjsjj@ipartners.co.kr>
2022-03-11 14:47:35 +0900

    chore: clean

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,14 +1,6 @@
 <template>
   <header>
     <h1><Logo :size="'medium'" /></h1>
-    <!-- <nav>
-      <ul>
-        <li><a href="">menu 1</a></li>
-        <li><a href="">menu 2</a></li>
-        <li><a href="">menu 3</a></li>
-        <li><a href="">menu 4</a></li>
-      </ul>
-    </nav> -->
     <div class="links">
       <LinkGithub />
     </div>
