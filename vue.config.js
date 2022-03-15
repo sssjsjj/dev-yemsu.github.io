1567d360941c5f08f856e2153796c6533f72c9f7
정수진 <sssjsjj@ipartners.co.kr>
2022-03-15 14:34:06 +0900

    build: prerenderer 설치/적용

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
+const path = require('path')
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
+
+const postPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'docs'),
+    routes: [
+        "/node-js-es6-plus-env-setting",
+        "/vue-cli-working-with-sass",
+        "/vue3-router",
+    ],
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      renderAfterElementExists: '#app'
+    }),
+  }),
+];
+
 module.exports = {
   outputDir: 'docs',
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...postPlugins);
+    }
+  },
   css: {
     loaderOptions: {
       scss: {
