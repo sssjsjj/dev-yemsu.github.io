93b702c1d2fba3d282e765be29553bd5391b307d
정수진 <sssjsjj@ipartners.co.kr>
2022-03-16 22:28:09 +0900

    build: add sitemap-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,35 @@
 const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
 
-const postPlugins = [
+const paths = [
+  {
+      path: '/',
+      lastmod: '2022-03-05',
+      priority: 0.9,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/node-js-es6-plus-env-setting',
+      lastmod: '2022-02-27',
+      priority: 0.9,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/vue-cli-working-with-sass',
+      lastmod: '2022-03-05',
+      priority: 0.9,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/vue3-router',
+      lastmod: '2022-03-10',
+      priority: 1.0,
+      changefreq: 'yearly'
+  }
+]
+
+const webpackPlugins = [
   new PrerenderSpaPlugin({
     staticDir: path.join(__dirname, 'docs'),
     routes: [
@@ -13,13 +41,14 @@ const postPlugins = [
       renderAfterElementExists: '#app'
     }),
   }),
+  new SitemapPlugin({ base: 'https://example.com', paths })
 ];
 
 module.exports = {
   outputDir: 'docs',
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(...postPlugins);
+      config.plugins.push(...webpackPlugins)
     }
   },
   css: {
@@ -28,5 +57,5 @@ module.exports = {
         additionalData: `@import "@/assets/style/global.scss";`
       }
     }
-  }
+  },
 }
