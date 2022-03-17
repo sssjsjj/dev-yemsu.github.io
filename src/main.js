34db25fc4e4996e5cda6ea8d2c43e549f6377943
정수진 <sssjsjj@ipartners.co.kr>
2022-03-17 16:26:14 +0900

    feat(build): vue meta install/setting

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createMetaManager } from 'vue-meta'
 
-const app = createApp(App)
-
-app.use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(createMetaManager())
+  .mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -14,4 +14,7 @@ module.exports = {
       }
     }
   },
+  transpileDependencies: [
+    'vue-meta',
+  ],
 }
