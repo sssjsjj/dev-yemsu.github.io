63034e0f22c0639bcbfc7b4bdbaa156dc5c97ec5
정수진 <sssjsjj@ipartners.co.kr>
2022-05-20 14:06:54 +0900

    feat: code highlight

--- src/utils/htmlConverter.js
+++ src/utils/htmlConverter.js
@@ -1,8 +1,9 @@
 import showdown from "showdown";
+import { mdHighlighter } from "./highlighter";
 
 const htmlConverter = (md) => {
   const converter = new showdown.Converter()
-  return converter.makeHtml(md)
+  return converter.makeHtml(mdHighlighter(md))
 }
 
 export default htmlConverter
