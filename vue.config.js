51ac8e6727e6f8c47727c61915cf09b833d56a53
정수진 <sssjsjj@ipartners.co.kr>
2022-05-20 15:42:03 +0900

    🚀

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const webpackPlugins = require('./webpack.plugin')
 
 module.exports = {
   outputDir: 'docs',
+  filenameHashing: false,
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(...webpackPlugins)
