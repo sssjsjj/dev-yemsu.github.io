09d1abb5c857cedb44dd953026410410c3229d1a
정수진 <=>
2023-01-03 17:22:01 +0900

    feat: 이전 포스팅 클 보이는 현상 수정

--- src/components/layout/Container.vue
+++ src/components/layout/Container.vue
@@ -25,12 +25,16 @@ export default {
   padding: 50px $container-padding-x-pc;
   box-sizing: content-box;
   @include pc {
+    $header-height: 121px;
+    min-height: calc(100vh - #{$header-height});
     &.size-narrow {
       width: $container-narrow-width;
       margin: 0 auto;
     }
   }
   @include mobile {
+    $header-height: 112px;
+    min-height: calc(100vh - #{$header-height});
     padding: 30px $container-padding-x-mo;
   }
 }
