7fb5bd7f471f1064aa5a9ed4c211ba25146e0574
정수진 <=>
2023-01-09 14:56:19 +0900

    fix: skeleton 의미 없는 마크업으로 수정

--- src/components/PostDetailSkeleton.vue
+++ src/components/PostDetailSkeleton.vue
@@ -1,20 +1,21 @@
 <template>
   <article>
     <div class="wrap-info">
-      <h2 class="title">title</h2>
+      <h2 class="skeleton skeleton-h2">title</h2>
     </div>
     <div class="article">
-      <h3>ddd</h3>
-      <p class="text-long">ddd</p>
-      <p class="text-normal">ddd</p>
-      <p class="text-short">ddd</p>
+      <!-- outliner 타겟에서 제외되기 위해 skeleton 클래스명 추가 -->
+      <div class="skeleton skeleton-h3">ddd</div>
+      <div class="skeleton skeleton-text long">ddd</div>
+      <div class="skeleton skeleton-text normal">ddd</div>
+      <div class="skeleton skeleton-text short">ddd</div>
     </div>
   </article>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/style/PostDetail.scss';
-h2, h3, p, li {
+.skeleton {
   background-color: var(--bg-gray);
   border-radius: 5px;
   color: transparent;
@@ -24,17 +25,19 @@ h2, h3, p, li {
   display: inline-block;
   color: transparent !important;
 }
-h2 {
+.skeleton-h2 {
   width: 80%;
   margin: 0 auto;
 }
-h3 {
+.skeleton-h3 {
   width: 40%;
 }
-.text-short {
-  width: 50%;
-}
-.text-normal {
-  width: 70%;
+.skeleton-text {
+  &.short {
+    width: 50%;
+  }
+  &.normal {
+    width: 70%;
+  }
 }
 </style>
